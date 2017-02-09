596079e598f33cefbdca9ce7f77e50cf3242d6ca
William Oliveira <sudowilliam@gmail.com>
2017-02-09 02:53:15 -0200

    Init file

---
+++ game/init.js
@@ -0,0 +1,20 @@
+// game instance
+var config = {
+    width: 500,
+    height: 200,
+    rendered: Phaser.AUTO,
+    resolution: window.devicePixelRatio,
+    antialias: false
+}
+
+var game = new Phaser.Game(config);
+
+// define game states
+game.state.add('boot', new Boot());
+game.state.add('load', new Load());
+game.state.add('menu', new Menu());
+game.state.add('play', new Play());
+game.state.add('win',  new Win());
+game.state.add('lose', new Lose());
+
+game.state.start('boot');
